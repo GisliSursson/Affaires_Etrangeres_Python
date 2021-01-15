630c3b19ae1bc037274f6b76a1f085b851988b14
Your Name <meynaud.victor@gmail.com>
2021-01-15 11:44:28 +0100

    Travail page recherche

---
+++ app/modeles/data_dict.py
@@ -0,0 +1,10 @@
+import csv
+import os
+from ..app import chemin_actuel
+
+# Script servant à charger les données du CSV dans un dict Python
+with open(os.path.join(chemin_actuel, "modeles", "codes_pays.csv"), "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    codes_dict = {}
+    for row in reader:
+        codes_dict[row[0]] = row[1]
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,27 @@
-from flask import render_template
+from flask import render_template, request
 from .app import app
-
+from .modeles.data_dict import codes_dict as data
 
 @app.route("/")
 def accueil():
     """ Route permettant l'affichage d'une page accueil
     """
-    return render_template("accueil.html", titre="Apli réseau diplo. : ")
+    return render_template("accueil.html", titre="Apli réseau diplo.")
 
+@app.route("/recherche")
+def recherche():
+	# Route permettant l'affichage de la page recherche généraliste
+    # Va chercher la valeur choisie par l'utilisateur dans l'HTML. Sera None en vas de valeur nulle.
+    type_recherche = request.args.get("type_voulu", None)
+    if type_recherche == 'pays':
+        type = 'par pays'
+        placeholder = 'Russie'
+    if type_recherche == 'ville':
+        type = 'par ville'
+        placeholder = 'Rome'
+    else:
+        type = 'librement'
+        placeholder = 'Votre recherche ici'
+    return render_template("recherche.html", type=type, placeholder=placeholder, titre="Recherche", data=data, type_recherche=type_recherche)
 
 
