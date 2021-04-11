efdbccf6773e6bad3e70820175fe339ebf1fa16a
Your Name <victor.meynaud@chartes.psl.eu>
2021-04-11 09:32:42 +0200

    search doc

--- app/search.py
+++ app/search.py
@@ -9,12 +9,14 @@ from .modeles.data_db import data as db
 
 # Définition du schéma du moteur de recherche. Le code du pays sera indexé et le contenu
 # sera retourné en fonction du mot indexé (stored). "City" sert à l'indexation des noms de ville.
-# "Name" sert à l'information de l'utilisateur. "Content" servira à produire le marqueur.
+# "Name" sert au type de représentation. Le contenu de "Content" servira à produire le marqueur de la carte.
 # Whoosh ne peut pas indexer des chaînes de caractères en UTF-8.
 schema = Schema(city=TEXT, name=TEXT(stored=True), content=TEXT(stored=True))
 
 # L'indexation n'est lancée que si le dossier "index" n'existe pas (la documentation Whoosh conseille de
-# stocker l'index dans un dossier comme cela).
+# stocker l'index dans un dossier comme cela). L'indexation n'est donc lancée qu'une seule fois dans le cycle de vie
+# de l'application installée en local. Si on refaisait l'indexation à chaque lancement de l'application, cela
+# gaspillerait des ressources.
 villes = []
 if not os.path.exists("index"):
     # Ce print ne doit s'afficher que lorsque l'index est écrit, c'est-à-dire lors du 1er lancement de l'application,
